8b2c4770fd3b8412a91438ec186182e8cabff292
bert-dev13 <ilacengelbert13@gmail.com>
2026-06-18 07:34:34 +0800

    incoming

--- src/data/incomingFiles.js
+++ src/data/incomingFiles.js
@@ -5,6 +5,7 @@ export const incomingFiles = [
   { name: 'Christine', link: 'https://drive.google.com/drive/folders/1IdbX3JsTbKfKBqv7CnExbrpsjSfMUShp?usp=sharing' },
   { name: 'Donnabel', link: 'https://drive.google.com/drive/folders/1s_MF00XCPEorxV1zDngr49IIv64AUMEU?usp=sharing' },
   { name: 'Jasmin', link: 'https://drive.google.com/drive/folders/1i79si4jnj3rnAP9kSsOt3LVWwIAjtj5D?usp=sharing' },
+  { name: 'Lanie', link: 'https://drive.google.com/drive/folders/1hWvgZTkD9iq2bOnwF3AXjL3NSGCUN7e5?usp=sharing' },
   { name: 'Medie', link: 'https://drive.google.com/drive/folders/1T2OIcQuEPS76VIE490zkxbWy314kahDU?usp=sharing' },
   { name: 'Jessper', link: 'https://drive.google.com/drive/folders/1_bO68umdMsH7WHHme04XgSWt1ztEefWs?usp=sharing' },
   { name: 'Engelbert', link: 'https://drive.google.com/drive/folders/15fwX-GpjHZGN_tIGLpPcBYYjj9Fd7bG0?usp=sharing' },
